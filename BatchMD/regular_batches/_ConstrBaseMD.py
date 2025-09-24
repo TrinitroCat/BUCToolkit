8aa2f437ea1037d57c0669731151e4a974b84b6d
TrinitroCat <siki2836700243@otlook.com>
2025-09-24 15:30:22 +0800

    1. implementing constrained MD algos.

--- BatchMD/regular_batches/_ConstrBaseMD.py
+++ BatchMD/regular_batches/_ConstrBaseMD.py
@@ -24,8 +24,9 @@ class _rConstrBase(_rBaseMD):
         time_step: float, time per step (ps).
         max_step: int, maximum steps.
         T_init: float, initial temperature, only to generate initial velocities of atoms by Maxwell-Boltzmann distribution. If V_init is given, T_init will be ignored.
-        constr_func: Callable, a tuple of Python functions as the constraint functions s_k that map R^n -> R^k and s_k(X) == 0. It takes one or more arguments, one of which must be a Tensor, and returns one Tensor with shape (k, ). `None` for identity function. see example below.
-        constr_val: th.Tensor,
+        constr_func: Callable, a tuple of Python functions as the constraint functions s_k(X) that map R^n -> R^k. It takes one or more arguments, one of which must be a Tensor, and returns one Tensor with shape (k, ). `None` for identity function. see example below.
+        constr_val: Callable[th.Tensor[1], th.Tensor] | th.Tensor, the constraint value of `constr_func`, i.e., constraints are `constr_func(X) = constr_val`.
+        By defining it as a callable constr_val = constr_val(t) where `t` is a scalar Tensor, it can be set to the time-dependent constraints.
         output_structures_per_step: int, output structures per output_structures_per_step steps.
         device: str|torch.device, device that program rum on.
         verbose: int, control the detailed degree of output information. 0 for silence, 1 for output Energy and Forces per step, 2 for output all structures.
@@ -59,6 +60,7 @@ class _rConstrBase(_rBaseMD):
             max_step: int,
             T_init: float = 298.15,
             constr_func: Callable | None = None,
+            constr_val: Callable[[th.Tensor], th.Tensor] | th.Tensor = None,
             output_file: str | None = None,
             output_structures_per_step: int = 1,
             device: str | th.device = 'cpu',
@@ -73,23 +75,54 @@ class _rConstrBase(_rBaseMD):
             device,
             verbose
         )
+        self.time_now_tens = th.tensor(self.time_now, dtype=th.float32, device=self.device)
         if constr_func is None:
-            constr_func = lambda X: X
+            constr_func = lambda X: 0.
+        if isinstance(constr_val, th.Tensor):
+            self.is_const_constr = True
+            self.constr_val = None
+            self.constr_val_now = constr_val.to(self.device)
+        elif isinstance(constr_val, Callable):
+            raise TypeError(f'Expected `constr_val` is Callable or torch.Tensor, but got {type(constr_val)}')
+        else:
+            self.is_const_constr = False
+            self.constr_val = constr_val
+            self.constr_val_now = constr_val(self.time_now_tens)
+
         if not isinstance(constr_func, Callable):
             raise TypeError(f"`constr_func` must consist of callable, but got {type(constr_func)}")
         self.constr_func = constr_func
         self.Q = None  # Q(n_batch, n_atoms * n_dim, n_constr)
+        self.q = th.zeros_like()
         self.sqrtM = None  # M^1/2, (n_batch, n_atoms, n_dim)
         self.negsqrtM = None  # M^-1/2
 
-    def _constr_func_wrapped(self, X: th.Tensor) -> Tuple[th.Tensor, th.Tensor]:
+    def _constr_func_wrapped(self, X):
         """
-        Just simply repeat the output of self.constr_func to use `has_aux` in th.func.jacrev to return the function values.
-        Returns: y, y
+        Manage the constraint functions.
+        Converting constraint functions into s_k(X) = constr_func(X) - constr_val, thereby constraints are s_k(X) = 0.
+        Returns:
 
         """
-        y = self.constr_func(X)
-        return y, y
+        y = self.constr_func(X) - self.constr_val_now
+        return y, y  # repeat the output to use `has_aux` in th.func.jacrev to return the function values
+
+    def _update_constr(self):
+        """
+        Update constraint function value.
+        Returns:
+
+        """
+        if self.is_const_constr:
+            pass
+        else:
+            self.time_now_tens.copy_(self.time_now)
+            with th.enable_grad():
+                self.time_now_tens.requires_grad_()
+                y = self.constr_val(self.time_now_tens)
+
+            self.constr_val_now = y.detach()
+            self.d_constr = th.autograd.grad(y, self.time_now_tens)[0]  #  d s_k(r)/dt
 
     def _jacobian(self, X: th.Tensor) -> th.Tensor:
         """
@@ -100,11 +133,12 @@ class _rConstrBase(_rBaseMD):
         Returns:
 
         """
+        self._update_constr()
         with th.enable_grad():
             jac, y = th.vmap(th.func.jacrev(self._constr_func_wrapped, has_aux=True))(X)
         if y.ndim != 2:
             raise ValueError(f'`constr_func` must return a 2D tensor of shape (n_batch, n_constr), but got {y.shape}.')
-        constr_err = th.max(y).item()
+        constr_err = th.max(th.abs(y)).item()
         if constr_err > 1e-4:
             self.logger.warning(f'WARNING: Too large constraint error: {constr_err:.4e}')
         self.logger.info(f'Max Constraint Error: {constr_err:.4e}')
@@ -125,9 +159,12 @@ class _rConstrBase(_rBaseMD):
         # (n_batch, n_constr, n_atoms, n_dim) * (n_batch, 1, n_atoms, n_dim) -flatten-> (n_batch, n_constr, n_atoms * n_dim)
         _jac = th.flatten(jac * self.negsqrtM.unsqueeze(1), -2, -1)
         _jacT = _jac.mT.contiguous()
-        Q = th.linalg.qr(_jacT).Q  # Q(n_batch, n_atoms * n_dim, n_constr) , R(n_batch, n_constr, n_constr)
+        Q, R = th.linalg.qr(_jacT)  # Q(n_batch, n_atoms * n_dim, n_constr) , R(n_batch, n_constr, n_constr)
 
-        self.Q = Q
+        if not self.is_const_constr:  # time-dependent constraints
+            th.linalg.solve_triangular(R.mT.contiguous(), self.d_constr, upper=False, out=self.q)
+
+        self.Q.copy_(Q)
 
     def _projected(self, X:th.Tensor) -> th.Tensor:
         """
