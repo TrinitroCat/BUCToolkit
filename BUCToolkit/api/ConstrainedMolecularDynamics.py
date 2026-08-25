1. Fixed the bug of `time_now`. Now it would be correctly re-initialised when recalling `run` for CMD methods. 2. Added and Correct the control of `require_fixman` by input file for CMD calculation. And now the argument names are unified to `N_IMAGES`, as the same as it in `NEB`. The old one of `NIMAGE` has been deprecated. 3. Updated the README and Manuals accordingly.

BUCToolkit/api/ConstrainedMolecularDynamics.py
@@ -34,7 +34,7 @@ class ConstrainedMolecularDynamics(_CONFIGS):
     Input file parameters: (Under the section `MD` in input files)
         ENSEMBLE: Literal[NVE, NVT], the ensemble for MD.
         CONSTR_MD_SCHEME: Literal[BLUE_MOON, SLOW_GROWTH], the scheme of constrained MD.
-        NIMAGE: int, the number of images in BLUE_MOON MD. SLOW_GROWTH uses it
+        N_IMAGES: int, the number of images in BLUE_MOON MD. SLOW_GROWTH uses it
                 for parallel copies of the initial structure; use 1 for one trajectory.
         THERMOSTAT: Literal[Langevin, VR, CSVR, Nose-Hoover], the thermostat type. only used for ENSEMBLE=NVT.
                     'VR' is Velocity Rescaling and 'CSVR' is Canonical Sampling Velocity Rescaling by Bussi et al. [1].
@@ -81,29 +81,44 @@ class ConstrainedMolecularDynamics(_CONFIGS):
         except KeyError:
             raise NotImplementedError(f'Unknown Ensemble {self.MD["ENSEMBLE"]}.')
         self.require_grad = self.MD.get('REQUIRE_GRAD', False)
-        # check modes
-        self.CMD_MODE = self.MD['CONSTR_MD_SCHEME']
-        if self.CMD_MODE == 'BLUE_MOON':
-            # interp images read & check
-            self.NIMAGE = self.MD['NIMAGE']
-        elif self.CMD_MODE == 'SLOW_GROWTH':
-            self.NIMAGE = self.MD.get('NIMAGE', 1)
-        else:
-            raise NotImplementedError(f'Unknown Constrained MD Scheme {self.CMD_MODE}.')
-        if self.NIMAGE <= 0:
-            raise RuntimeError(f'Images of configurations must be greater than 0, but got {self.NIMAGE}.')
 
-        self.MD_config = {'time_step': self.MD.get('TIME_STEP', 1),
-                          'max_step': self.MD.get('MAX_STEP'),
-                          'T_init': self.MD.get('T_INIT', 298.15),
-                          'output_structures_per_step': self.MD.get('OUTPUT_COORDS_PER_STEP', 1),
-                          'device': self.DEVICE,
-                          'verbose': self.VERBOSE}
+        self.MD_config = {
+            'time_step': self.MD.get('TIME_STEP', 1),
+            'max_step': self.MD.get('MAX_STEP'),
+            'T_init': self.MD.get('T_INIT', 298.15),
+            'constr_threshold': self.MD.get('CONSTR_THRESHOLD', 1.e-5),
+            'require_fixman': self.MD.get('REQUIRE_FIXMAN', 'auto'),  # 'auto' means True for blue-moon and False otherwise.
+            'output_structures_per_step': self.MD.get('OUTPUT_COORDS_PER_STEP', 1),
+            'device': self.DEVICE,
+            'verbose': int(self.VERBOSE)
+        }
+        if isinstance(self.MD_config['require_fixman'], str) and (self.MD_config['require_fixman']).lower() != 'auto':
+            raise ValueError(
+                f"Invalid value of `require_fixman` of {self.MD_config['require_fixman']}. "
+                f"It must be either boolean or 'str'. "
+            )
+        is_auto_fixman = (not isinstance(self.MD_config['require_fixman'], bool))
         if self.SAVE_PREDICTIONS:
             self.MD_config['output_file'] = self.PREDICTIONS_SAVE_FILE
         if self.MD['ENSEMBLE'] == 'NVT':
             self.MD_config['thermostat'] = self.MD.get('THERMOSTAT', 'CSVR')
             self.MD_config['thermostat_config'] = self.MD.get('THERMOSTAT_CONFIG', dict())
+
+        # check modes
+        self.CMD_MODE = self.MD['CONSTR_MD_SCHEME']
+        if self.CMD_MODE == 'BLUE_MOON':
+            # interp images read & check
+            self.N_IMAGES = self.MD['N_IMAGES']
+            if is_auto_fixman: self.MD_config['require_fixman'] = True # Blue-Moon ensemble require fixman term
+        elif self.CMD_MODE == 'SLOW_GROWTH':
+            # Slow-growth can use Jarzynski approach, which makes multiple replica
+            #   Hence, use this variable to apply this replica scheme in parallel.
+            self.N_IMAGES = self.MD.get('N_IMAGES', 1)
+            if is_auto_fixman: self.MD_config['require_fixman'] = False  # Jarzynski needs not it
+        else:
+            raise NotImplementedError(f'Unknown Constrained MD Scheme {self.CMD_MODE}.')
+        if self.N_IMAGES <= 0:
+            raise RuntimeError(f'Images of configurations must be greater than 0, but got {self.N_IMAGES}.')
 
     def set_constr_func(self, constr_func: Callable) -> None:
         self.constr_func = constr_func
@@ -128,12 +143,10 @@ class ConstrainedMolecularDynamics(_CONFIGS):
                 _model.load_state_dict(chk_data['model_state_dict'], strict=False)
             else:
                 _model.load_state_dict(self.param, self.is_strict, self.is_assign)
-            epoch_now = chk_data['epoch']
         elif self.START == 'from_scratch' or self.START == 0:
             self.logger.warning(
                 'WARNING: The model was not read the trained parameters from checkpoint file. I HOPE YOU KNOW WHAT YOU ARE DOING!'
             )
-            epoch_now = 0
             if self.param is not None:
                 _model.load_state_dict(self.param, self.is_strict, self.is_assign)
         else:
@@ -278,10 +291,19 @@ class ConstrainedMolecularDynamics(_CONFIGS):
             if isinstance(self.log_handler, logging.FileHandler):
                 self.log_handler.close()
 
-    def _run_cmd_blue_moon(self, mole_dynam, model_wrap,
-                           get_batch_size, get_cell_vec, get_atomic_number,
-                           get_indx, get_fixed_mask, get_batch_indx,
-                           get_init_veloc, rebatched_graph):
+    def _run_cmd_blue_moon(
+            self,
+            mole_dynam,
+            model_wrap,
+            get_batch_size,
+            get_cell_vec,
+            get_atomic_number,
+            get_indx,
+            get_fixed_mask,
+            get_batch_indx,
+            get_init_veloc,
+            rebatched_graph
+    ):
         """BLUE_MOON scheme: ISFS loader, IS/FS pairs, linear interpolation."""
         val_set: Any = self._data_loader(
             self.TRAIN_DATA, self.BATCH_SIZE,
@@ -346,14 +368,14 @@ class ConstrainedMolecularDynamics(_CONFIGS):
                 else:
                     X_is = dataIS.nodes['atom'].data['pos']
                     X_fs = dataFS.nodes['atom'].data['pos']
-                X_init_ = linear_interpolation_tens(X_is, X_fs, self.NIMAGE)
+                X_init_ = linear_interpolation_tens(X_is, X_fs, self.N_IMAGES)
                 origin_elem_list = get_atomic_number(dataIS)
                 dataIS = rebatched_graph(dataIS, X_init_)
 
                 mole_dynam.run(
                     model_wrap.Energy, X_init_,
-                    [origin_elem_list] * self.NIMAGE,
-                    Cell_vector=np.repeat(_cell, self.NIMAGE, axis=0),
+                    [origin_elem_list] * self.N_IMAGES,
+                    Cell_vector=np.repeat(_cell, self.N_IMAGES, axis=0),
                     V_init=get_init_veloc(dataIS),
                     grad_func=model_wrap.Grad,
                     func_args=(dataIS,), grad_func_args=(dataIS,),
@@ -419,14 +441,14 @@ class ConstrainedMolecularDynamics(_CONFIGS):
                 else:
                     X_is = dataIS.nodes['atom'].data['pos']
                 # For SLOW_GROWTH: copy the same structure N_Images times
-                X_init_ = X_is.unsqueeze(0).expand(self.NIMAGE, *X_is.shape)
+                X_init_ = X_is.unsqueeze(0).expand(self.N_IMAGES, *X_is.shape)
                 origin_elem_list = get_atomic_number(dataIS)
                 dataIS = rebatched_graph(dataIS, X_init_)
 
                 mole_dynam.run(  #  Model not support the regular batch, although our code do.
                     model_wrap.Energy, X_init_,
-                    [origin_elem_list] * self.NIMAGE,
-                    Cell_vector=np.repeat(_cell, self.NIMAGE, axis=0),
+                    [origin_elem_list] * self.N_IMAGES,
+                    Cell_vector=np.repeat(_cell, self.N_IMAGES, axis=0),
                     V_init=get_init_veloc(dataIS),
                     grad_func=model_wrap.Grad,
                     func_args=(dataIS,), grad_func_args=(dataIS,),